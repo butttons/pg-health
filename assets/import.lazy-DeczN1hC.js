100ce34c969fc7e9b52b0edc5fc416f860d83ac2
butttons <butttons@users.noreply.github.com>
2024-12-27 12:50:00 +0000

    deploy: a190b97e02cbb51ce82c02d82ef87efc32e01057

--- assets/import.lazy-DeczN1hC.js
+++
@@ -1,1 +0,0 @@
-import{a5 as t,j as o}from"./index-eg-OLtk0.js";import{S as e}from"./setup-DEVx9znS.js";import"./index-BGCDRvAS.js";import"./alert-BizdAWp3.js";import"./card-Dk9gIrxK.js";const s=t("/import")({component:r});function r(){return o.jsx("div",{className:"container p-4 mx-auto",children:o.jsx(e,{})})}export{s as Route};
