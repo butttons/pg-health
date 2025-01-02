49feb5d6232d5b74c08e8206af9b38e01be7a0c6
butttons <butttons@users.noreply.github.com>
2025-01-02 14:25:03 +0000

    deploy: c790ae034229ad312ad8a42a24f066e0f0c5c926

---
+++ assets/import.lazy-U4uLbXzd.js
@@ -0,0 +1,1 @@
+import{a6 as t,j as o}from"./index-eoAsxTvW.js";import{S as e}from"./setup-DRG_QWP6.js";import"./index-DXALhEVm.js";import"./card-DjbHnAJP.js";const a=t("/import")({component:r});function r(){return o.jsx("div",{className:"container p-4 mx-auto",children:o.jsx(e,{})})}export{a as Route};
